ff6290c0a2cad186294bacb23ee2e7ea2c4d77ee
Peter Salvi <salvi@iit.bme.hu>
2025-04-28 13:39:33 +0200

    Numeric gradient computation

--- examples/rockwood-owen.scm
+++ examples/rockwood-owen.scm
@@ -1,10 +1,32 @@
 (load "vectors.scm")                    ; basic vector operations
 
+;;; Options
+
+(define numeric-gradient? #t)           ; much slower when turned on
+
+(define epsilon 1e-7)                   ; used in numeric gradient computation
+
+;;; Utilities
+
 ;;; Safely descale the vector: return (0 0 0) when dividing by 0
 (define (v/ u x)
   (if (< (abs x) 1e-8)
       '(0 0 0)
       (v* u (/ x))))
+
+(define (with-gradient f)
+  (if numeric-gradient?
+      (let ((deltas (map (lambda (u)
+                           (v* u epsilon))
+                         '((1 0 0) (0 1 0) (0 0 1)))))
+        (lambda (p)
+          (let ((fp (f p)))
+            (cons fp
+                  (map (lambda (dp)
+                         (/ (- (f (v+ p dp)) fp) epsilon))
+                       deltas)))))
+      (lambda (p)
+        (cons (f p) '(0 0 0)))))
 
 ;;; Primitives returning (value . gradient)
 
@@ -31,39 +53,38 @@
 
 ;;; The Rockwood-Owen approximative rolling ball blend
 (define (blend f1 f2 r)
-  (lambda (p)
-    (let* ((fx1 (f1 p))
-           (fx2 (f2 p))
-           (P1 (car fx1))
-           (P2 (car fx2))
-           (cos-theta (scalar-product (v/ (cdr fx1) (vlength (cdr fx1)))
-                                      (v/ (cdr fx2) (vlength (cdr fx2)))))
-           (sin-theta (sqrt (- 1 (* cos-theta cos-theta))))
-           (d1 (- r (* (- r P2) cos-theta)))
-           (d2 (- r (* (- r P1) cos-theta))))
-      (cons (if (and (< P1 d1) (< P2 d2)) ; on R
-                (if (< (abs sin-theta) 1e-8)
-                    0                   ; kutykurutty
-                    (- r (/ (sqrt (+ (* (- r P1) (- r P1))
-                                     (* (- r P2) (- r P2))
-                                     (* -2 (- r P1) (- r P2) cos-theta)))
-                            sin-theta)))
-                (min P1 P2))
-            '(0 0 0)))))                ; kutykurutty
+  (with-gradient
+   (lambda (p)
+     (let* ((fx1 (f1 p))
+            (fx2 (f2 p))
+            (P1 (car fx1))
+            (P2 (car fx2))
+            (cos-theta (scalar-product (v/ (cdr fx1) (vlength (cdr fx1)))
+                                       (v/ (cdr fx2) (vlength (cdr fx2)))))
+            (sin-theta (sqrt (- 1 (* cos-theta cos-theta))))
+            (d1 (- r (* (- r P2) cos-theta)))
+            (d2 (- r (* (- r P1) cos-theta))))
+       (if (and (< P1 d1) (< P2 d2))    ; on R
+           (if (< (abs sin-theta) 1e-8)
+               0                        ; kutykurutty
+               (- r (/ (sqrt (+ (* (- r P1) (- r P1))
+                                (* (- r P2) (- r P2))
+                                (* -2 (- r P1) (- r P2) cos-theta)))
+                       sin-theta)))
+           (min P1 P2))))))
 
 (define (se-blend f1 f2 r)
-  (lambda (p)
-    (let* ((fx1 (f1 p))
-           (fx2 (f2 p))
-           (P1 (car fx1))
-           (P2 (car fx2))
-           (cos-theta (scalar-product (v/ (cdr fx1) (vlength (cdr fx1)))
-                                      (v/ (cdr fx2) (vlength (cdr fx2)))))
-           (range (* r (- 1 cos-theta))))
-      (cons (- 1 (expt (max 0 (- 1 (/ P1 range))) 2)
-                 (expt (max 0 (- 1 (/ P2 range))) 2))
-            '(0 0 0)))))                ; kutykurutty
-
+  (with-gradient
+   (lambda (p)
+     (let* ((fx1 (f1 p))
+            (fx2 (f2 p))
+            (P1 (car fx1))
+            (P2 (car fx2))
+            (cos-theta (scalar-product (v/ (cdr fx1) (vlength (cdr fx1)))
+                                       (v/ (cdr fx2) (vlength (cdr fx2)))))
+            (range (* r (- 1 cos-theta))))
+       (- 1 (expt (max 0 (- 1 (/ P1 range))) 2)
+          (expt (max 0 (- 1 (/ P2 range))) 2))))))
 
 (define (complement f)
   (lambda (p)
